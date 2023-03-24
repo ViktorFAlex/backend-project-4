7f23ba7f06eefa062a4de8a5b1f9f09e3b9f1f6b
ViktorFAlex <viktor.f.alex@gmail.com>
2023-03-24 18:30:19 +0800

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,35 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeEach } from '@jest/globals';
+import nock from 'nock';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
-import pageLoader from '../bin/pageLoader';
+import fs from 'fs/promises';
+import os from 'os';
+import loadPage from '../src/index.js';
+import buildFileName from '../src/helpers/buildFileName.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+
+let tmpDirPath;
+
+beforeEach(async () => {
+  const dirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  tmpDirPath = path.join(dirPath);
+});
+
+test('loadPageContent', async () => {
+  const url = 'https://page-loader.hexlet.repl.co/';
+  const fileName = buildFileName(url);
+  const fixtureFileName = fileName.replace(/html/, 'txt');
+  await fs.readFile(getFixturePath(fixtureFileName), 'utf-8');
+  const data = await readFile(fixtureFileName);
+
+  nock(url).get('/').reply(200, data);
+
+  await loadPage(url, path.join(tmpDirPath));
+
+  const fileData = await fs.readFile(path.join(tmpDirPath, fileName), 'utf-8');
+  expect(fileData).toStrictEqual(data);
+});
--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -1,35 +1,18 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import axios from 'axios';
-import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs/promises';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import loadPage from '../src/index.js';
 
 program
   .name('page-loader')
   .version('1.0.0', '-V, --version', 'output the current version')
   .description('Download content from webpage and save it as assets.')
   .helpOption('-h, --help', 'output help message')
-  .option('-o, --output [dir]', `output dir (default: '${__dirname}')`, `${__dirname}`)
+  .option('-o, --output [dir]', `output dir (default: '${process.cwd()}')`)
   .argument('<url>')
   .action((url) => {
-    const outputDir = program.opts().output;
-
-    const fileName = url
-      .replace(/(http|https):\/\//, '')
-      .replace(/[^a-zA-Z\d]/g, '-')
-      .replace(/\b$/, '.html');
-
-    axios.get(url).then((response) => {
-      const dirPath = path.join(__dirname, outputDir);
-      const filePath = path.join(dirPath, fileName);
-      fs.mkdir(path.join(__dirname, outputDir), { recursive: true })
-        .then(() => fs.writeFile(filePath, response.data))
-        .then(() => console.log('Done!'));
-    });
+    const outputDir = path.join(process.cwd(), program.opts().output);
+    loadPage(url, outputDir).then(() => console.log('Done!'));
   });
 
 program.parse();
