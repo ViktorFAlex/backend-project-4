975c648ec7fbc37a17b3284f58c63a45b66910a0
ViktorFAlex <viktor.f.alex@gmail.com>
2023-03-26 19:23:23 +0800

    add links handler

--- src/index.js
+++ src/index.js
@@ -1,50 +1,25 @@
 import axios from 'axios';
-import * as cheerio from 'cheerio';
 import path from 'path';
 import fs from 'fs/promises';
 import buildFileName from './helpers/buildFileName.js';
+import manipulateDomLinks from './helpers/manipulateDomLinks.js';
 
 const loadPage = (url, dirPath) => {
   const fileName = buildFileName(url);
   const filePath = path.join(dirPath, fileName);
   return axios.get(url).then((response) => {
-    const $ = cheerio.load(response.data, {
-      xmlMode: true,
-      decodeEntities: false,
-    });
-    const $imgs = $('img');
-    const imgPromises = [];
-    const { hostname } = new URL(url);
     const imgFolderName = buildFileName(url, '_files');
     const imgFolderPath = path.join(dirPath, imgFolderName);
-    $imgs
-      .toArray()
-      .filter(({ attribs: { src } }) => /^\//.test(src))
-      .forEach((element) => {
-        const { attribs } = element;
-        const { dir, name, ext } = path.parse(attribs.src);
-        const imgFileName = buildFileName(path.join(hostname, dir, name), ext);
-        const newSrc = path.join(imgFolderName, imgFileName);
-        const imgUrl = new URL(attribs.src, url);
-        const imgPath = path.join(imgFolderPath, imgFileName);
-        imgPromises.push({ imgUrl, imgPath });
-        attribs.src = newSrc;
-      });
+    const [$, imgPromises] = manipulateDomLinks(response.data, url, imgFolderName, imgFolderPath);
+
     return fs
       .mkdir(imgFolderPath, { recursive: true })
-      .then(() =>
-        fs
-          .writeFile(filePath, $.html())
-          .then(() =>
-            Promise.all(
-              imgPromises.map(({ imgUrl, imgPath }) =>
-                axios
-                  .get(imgUrl, { responseType: 'arraybuffer' })
-                  .then(({ data }) => fs.writeFile(imgPath, data)),
-              ),
-            ),
-          ),
-      );
+      .then(() => fs.writeFile(filePath, $.html())
+        .then(() => Promise.all(
+          imgPromises.map(({ imgUrl, imgPath }) =>
+            axios.get(imgUrl, { responseType: 'arraybuffer' })
+              .then(({ data }) => fs.writeFile(imgPath, data))),
+        )));
   });
 };
 
