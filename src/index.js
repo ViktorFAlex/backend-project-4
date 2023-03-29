8973ee544dd07ad86be07e73a03d586d61ac2dc1
ViktorFAlex <viktor.f.alex@gmail.com>
2023-03-29 15:43:11 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const loadPage = (url, dirPath) => {
         .mkdir(folderPath, { recursive: true })
         .then(() => {
           appLog(`Folder created at ${folderPath}`);
-          return fs.writeFile(htmlFilePath, $.html());
+          return fs.writeFile(htmlFilePath, $.xml());
         })
         .then(() => {
           const spinners = promises.map(({ fileUrl, filePath, type }) => {
